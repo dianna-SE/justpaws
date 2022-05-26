3015ca87572f1a95a19fede3c1d1f4c33467c9be
dianna-SE <diannapham.se@gmail.com>
2022-05-26 02:31:51 -0700

    minor changes to index.html

--- static/js/app.js
+++ static/js/app.js
@@ -7,8 +7,7 @@ const app = Vue.createApp({
             anyText: '',
             showSearch: false,
             showUpload: false,
-            showNavbar: true,
-            lastScrollPosition: 0
+            showSuggested: false,
         };
     },
     methods: {
@@ -24,26 +23,10 @@ const app = Vue.createApp({
         toggleUpload() {
             this.showUpload = !this.showUpload
         },
-        onScroll () {
-            const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop
-            if (currentScrollPosition < 0) {
-              return
-            }
-            // Stop executing this function if the difference between
-            // current scroll position and last scroll position is less than some offset
-            if (Math.abs(currentScrollPosition - this.lastScrollPosition) < 60) {
-              return
-            }
-            this.showNavbar = currentScrollPosition < this.lastScrollPosition
-            this.lastScrollPosition = currentScrollPosition
-          }
+        toggleSuggested() {
+            this.showSuggested = !this.showSuggested
+        },
     },
-            mounted () {
-            window.addEventListener('scroll', this.onScroll)
-        },
-        beforeDestroy () {
-            window.removeEventListener('scroll', this.onScroll)
-        },
 })
 
 app.mount('#app');
